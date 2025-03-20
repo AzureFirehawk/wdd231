83c0ffca4f7bf250200f2dcd70526dac60c1d21a
AzureFirehawk <madison.thomas094@gmail.com>
2025-03-20 11:15:54 -0500

    week 2 learning activities

---
+++ scripts/prophets.js
@@ -0,0 +1,20 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+function displayProphets(prophets) {
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let order = document.createElement('p');
+    })
+}
