2e90440d7485b29da567f935b77677d987e0ae58
AzureFirehawk <madison.thomas094@gmail.com>
2025-03-21 13:17:02 -0500

    Learning Activities

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -4,17 +4,34 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    // console.table(data.prophets);
     displayProphets(data.prophets);
 }
 
-function displayProphets(prophets) {
+getProphetData();
+
+const displayProphets = (prophets) => {
     prophets.forEach(prophet => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
-        let birthDate = document.createElement('p');
+        let birthDate = document.createElement('p')
         let birthPlace = document.createElement('p');
-        let order = document.createElement('p');
+
+        card.setAttribute('class', 'card');
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        card.appendChild(h2);
+        card.appendChild(portrait);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        cards.appendChild(card);
     })
 }
