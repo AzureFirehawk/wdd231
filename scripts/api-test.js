cf905aabc2a1df34ec258bea7d526bb49790db79
AzureFirehawk <madison.thomas094@gmail.com>
2025-03-25 10:50:49 -0500

    api fetch learning assignment

---
+++ scripts/api-test.js
@@ -0,0 +1,29 @@
+const temp = document.querySelector('#current-temp');
+const icon = document.querySelector('#weather-icon');
+const desc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=536a7f86e83f64be0db592241461f30f';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    temp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    icon.setAttribute('src', iconsrc);
+    icon.setAttribute('alt', data.weather[0].description);
+    desc.textContent = data.weather[0].description;
+}
