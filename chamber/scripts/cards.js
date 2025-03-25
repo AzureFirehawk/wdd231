89bced469bb80ed8f47547d28de47f296435e886
AzureFirehawk <madison.thomas094@gmail.com>
2025-03-25 16:33:48 -0500

    List display toggle added, image size reduced

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,5 +1,8 @@
 const business = './data/members.json'
 const cards = document.querySelector('#cards');
+const lists = document.querySelector('#list');
+const grid = document.querySelector('#grid-button');
+const list = document.querySelector('#list-button');
 
 async function getBusinessData() {
     const response = await fetch(business);
@@ -41,35 +44,16 @@ const displayBusiness = (members) => {
     })
 }
 
-const businessSpotlight = (members) => {
-    let unique = new Set();
-    while (unique.size < 3) {
-        unique.add(members[Math.floor(Math.random() * members.length)]);
-    }
-    unique.forEach(member => {
-        let card = document.createElement('section');    
-        let h2 = document.createElement('h2');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let phone = document.createElement('p');
-        let image = document.createElement('img');
+grid.addEventListener('click', () => {
+    cards.classList.add('grid');
+    cards.classList.remove('list');
+    grid.classList.add('active');
+    list.classList.remove('active');
+})
 
-        card.setAttribute('class', 'card');
-        h2.textContent = `${member.name}`;
-        p1.textContent = `${member.address1}`;
-        p2.textContent = `${member.address2}`;
-        phone.textContent = `${member.phone}`;
-        image.setAttribute('src', member.image);
-        image.setAttribute('alt', `${member.name} logo`);
-        image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '200');
-        image.setAttribute('height', '200');
-
-        card.appendChild(image);
-        card.appendChild(h2);
-        card.appendChild(p1);
-        card.appendChild(p2);
-        card.appendChild(phone);
-        spotlight.appendChild(card);
-    })
-}
+list.addEventListener('click', () => {
+    cards.classList.add('list');
+    cards.classList.remove('grid');
+    list.classList.add('active');
+    grid.classList.remove('active');
+})
