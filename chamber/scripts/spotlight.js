cec1ab2c696a1dc7a0eddcdf94feab5d2f8699cf
AzureFirehawk <madison.thomas094@gmail.com>
2025-03-25 14:58:00 -0500

    Business Spotlight and current weather

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,45 +1,15 @@
 const business = './data/members.json'
-const cards = document.querySelector('#cards');
+const spotlight = document.querySelector('#spotlight');
 
 async function getBusinessData() {
     const response = await fetch(business);
     const data = await response.json();
-    displayBusiness(data.members);
+    const filtered = data.members.filter(member => member.membership > 1);
+    businessSpotlight(filtered);
 }
 
 getBusinessData();
 
-const displayBusiness = (members) => {
-    members.forEach(member => {
-        let card = document.createElement('section');
-        let text = document.createElement('div');
-        let h2 = document.createElement('h2');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let phone = document.createElement('p');
-        let image = document.createElement('img');
-
-        card.setAttribute('class', 'card');
-        h2.textContent = `${member.name}`;
-        p1.textContent = `${member.address1}`;
-        p2.textContent = `${member.address2}`;
-        phone.textContent = `${member.phone}`;
-        image.setAttribute('src', member.image);
-        image.setAttribute('alt', `${member.name} logo`);
-        image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '200');
-        image.setAttribute('height', '200');
-
-        card.appendChild(image);
-        card.appendChild(h2);
-        text.appendChild(p1);
-        text.appendChild(p2);
-        text.appendChild(phone);
-        card.appendChild(text);
-    
-        cards.appendChild(card);
-    })
-}
 
 const businessSpotlight = (members) => {
     let unique = new Set();
@@ -53,6 +23,8 @@ const businessSpotlight = (members) => {
         let p2 = document.createElement('p');
         let phone = document.createElement('p');
         let image = document.createElement('img');
+        let link = document.createElement('a');
+        let membership = document.createElement('p');
 
         card.setAttribute('class', 'card');
         h2.textContent = `${member.name}`;
@@ -65,11 +37,25 @@ const businessSpotlight = (members) => {
         image.setAttribute('width', '200');
         image.setAttribute('height', '200');
 
+        link.setAttribute('href', member.url);
+        link.setAttribute('target', '_blank');
+        link.textContent = `${member.urlText}`;
+        if (member.membership == 1) {
+            membership.textContent = "Membership: Member";
+        } else if (member.membership == 2) {
+            membership.textContent = "Membership: Silver";
+        } else if (member.membership == 3) {
+            membership.textContent = "Membership: Gold";
+            
+        }
+
         card.appendChild(image);
         card.appendChild(h2);
         card.appendChild(p1);
         card.appendChild(p2);
         card.appendChild(phone);
+        card.appendChild(membership);
+        card.appendChild(link);
         spotlight.appendChild(card);
     })
 }
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,1 +1,30 @@
-const currentTemp = document.querySelector('#current-temp');
+const temp = document.querySelector('#current-temp');
+const icon = document.querySelector('#weather-icon');
+const desc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35.139236&lon=-97.394753&units=imperial&appid=536a7f86e83f64be0db592241461f30f';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    temp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    icon.setAttribute('src', iconsrc);
+    icon.setAttribute('alt', data.weather[0].description);
+    desc.innerHTML = `<strong>${data.weather[0].description.toUpperCase()}</strong>`;
+}
