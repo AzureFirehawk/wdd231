7afefc46191fb5f782965dfe96dae3fe5d136596
AzureFirehawk <madison.thomas094@gmail.com>
2025-04-11 17:16:59 -0500

    home page initial content and styling

---
+++ project/scripts/home-slides.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('#hero .slide');
+let currentSlide = 0;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(showNextSlide, 7000); // Change image every 4 seconds
